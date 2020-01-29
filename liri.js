34116e474d00282db0cdd24d8e462907a6217e08
Anthony Pricer <anthony.pricer@outlook.com>
2020-01-28 21:14:16 -0600

    liri

--- liri.js
+++ liri.js
@@ -3,3 +3,9 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+var Spotify = require("node-spotify-api");
+
+var axios = require("axios");
+
+var fs = require("fs");
