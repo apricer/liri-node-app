52122d5b191f9147a0c0ef31fd6fd0cad4211db3
Anthony Pricer <anthony.pricer@outlook.com>
2020-02-13 19:10:34 -0600

    liri

--- liri.js
+++ liri.js
@@ -8,4 +8,72 @@ var Spotify = require("node-spotify-api");
 
 var axios = require("axios");
 
+var moment = require("moment");
+
 var fs = require("fs");
+
+var getMeSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "What's my age again";
+    }
+
+    spotify.search(
+        {
+            type: "track",
+            query: songName
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
+
+var getMyBands = function (artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL).then(
+        function (response) {
+            var jsonData = response.data;
+
+            if (!jsonData.length) {
+                console.log("No results found for " + artist);
+                return;
+            }
+
+            console.log("Upcoming concerts for " + artist + ":");
+
+            for (var i = 0; i < jsonData.length; i++) {
+                var show = jsonData[i];
+
+                // Print data about each concert
+                // If a concert doesn't have a region, display the country instead
+                // Use moment to format the date
+                console.log(
+                    show.venue.city +
+                    "," +
+                    (show.venue.region || show.venue.country) +
+                    " at " +
+                    show.venue.name +
+                    " " +
+                    moment(show.datetime).format("MM/DD/YYYY")
+                );
+            }
+        }
+    );
+};
+
+runThis(process.argv[2], process.argv.slice(3).join(" "));
